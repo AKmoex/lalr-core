b3e9861fcc4beda64e3e283eb76f9e5b007bb967
AKmoex <akmoex@gmail.com>
2021-05-23 15:49:20 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/lalr_core.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lalr_core.dir/main.cpp.obj"
+  "lalr_core.exe"
+  "lalr_core.exe.manifest"
+  "lalr_core.pdb"
+  "liblalr_core.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lalr_core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
